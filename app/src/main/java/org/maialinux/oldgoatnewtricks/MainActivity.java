2a6cdf88d23178969b42d691b6e097aea65357ab
Ruggero Cucchiani <ruggero.cucchiani@exertisztorm.com>
2018-04-17 09:50:47 +0200

    create basic activity
    Basic activity. Counts down five minutes. Ring.
    Reset.
    Works in the background.
    All accidental.
    Locked to portrait to avoid reset by switching orientation.

--- app/src/main/java/org/maialinux/oldgoatnewtricks/MainActivity.java
+++ app/src/main/java/org/maialinux/oldgoatnewtricks/MainActivity.java
@@ -2,11 +2,6 @@ package org.maialinux.oldgoatnewtricks;
 
 
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -23,7 +18,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
 
-    private static final long INTERVAL = 3000;
+    private static final long INTERVAL = 300000; // 5 minutes
 
 
     TextView timerTextView;
@@ -31,13 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     public Handler timerHandler;
-
     long expirationTime;
 
     Ringtone ringtone;
-    float mAccel; // acceleration apart from gravity
-    float mAccelCurrent; // current acceleration including gravity
-    float mAccelLast; // last acceleration including gravity
 
     String[] log = new String[5];
     
@@ -46,16 +37,9 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
 
 
-            if (mAccel >= 0.5) {
-                if (ringtone.isPlaying()) {
-                    ringtone.stop();
-                }
-                expirationTime = System.currentTimeMillis() + INTERVAL;
-            }
-
             long millis = expirationTime - System.currentTimeMillis();
-            long seconds = Math.round(millis/1000);
-
+            long minutes = millis/60000;
+            long seconds = millis/1000 - (minutes*60); // Remainder
 
             if (millis <= 0) {
                 timerHandler.removeCallbacks(timerRunnable);
@@ -66,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Calendar calendar = Calendar.getInstance();
                 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                String current = String.format("%s: Accel = %f, Seconds left = %s", format.format(calendar.getTime()), mAccel, seconds);
+                String current = String.format("%s: %sm %ss left", format.format(calendar.getTime()), minutes, seconds);
                 System.arraycopy(log, 0, log, 1, 4);
                 log[0] = current;
                 String text = "";
@@ -75,34 +59,11 @@ public class MainActivity extends AppCompatActivity {
                 }
                 timerTextView.setText(text);
 
-                timerHandler.postDelayed(this, 1000);
+                timerHandler.postDelayed(this, 30000); //Check every 30 seconds.
             }
 
         }
     };
-
-    private final SensorEventListener sensorEventListener = new SensorEventListener() {
-
-
-        @Override
-        public void onSensorChanged(SensorEvent se) {
-            float x = se.values[0];
-            float y = se.values[1];
-            float z = se.values[2];
-            mAccelLast = mAccelCurrent;
-            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
-            float delta = mAccelCurrent - mAccelLast;
-            mAccel = Math.abs(mAccel * 0.9f + delta); // perform low-cut filter
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
-
-
 
 
     @Override
@@ -111,21 +72,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timerTextView = findViewById(R.id.main_activity_view);
-
-        mAccel = 0.0f;
-        mAccelCurrent = SensorManager.GRAVITY_EARTH;
-        mAccelLast = SensorManager.GRAVITY_EARTH;
-
-
         timerHandler = new Handler();
         expirationTime = System.currentTimeMillis() + INTERVAL;
 
         Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
-
-        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-
 
         timerRunnable.run();
 
@@ -148,6 +99,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
 
 
 
