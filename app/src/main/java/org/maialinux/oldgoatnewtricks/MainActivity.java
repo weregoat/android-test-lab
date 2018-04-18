849b250cc3421c3c0bbd0e142d38387f2ef0ee01
Ruggero Cucchiani <ruggero.cucchiani@exertisztorm.com>
2018-04-18 10:30:34 +0200

    add time exclusion
    The application should expect input only during a certain time window.

--- app/src/main/java/org/maialinux/oldgoatnewtricks/MainActivity.java
+++ app/src/main/java/org/maialinux/oldgoatnewtricks/MainActivity.java
@@ -4,7 +4,6 @@ package org.maialinux.oldgoatnewtricks;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Handler;
@@ -22,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     private static final long INTERVAL = 3600000; // One hour
+    private static final String SLEEP_TIME = "14:00";
+    private static final String WAKE_TIME = "11:00";
+
+
 
 
     TextView timerTextView;
@@ -29,37 +32,43 @@ public class MainActivity extends AppCompatActivity {
     Uri ringtoneUri;
 
 
+
     public Handler timerHandler;
     long expirationTime;
 
     String[] log = new String[5];
+
     
     Runnable timerRunnable = new Runnable() {
         @Override
         public void run() {
 
 
-            long millis = expirationTime - System.currentTimeMillis();
+            if (sleepTime() == false) {
+                long millis = expirationTime - System.currentTimeMillis();
 
-            long minutes = millis/60000;
-            long seconds = millis/1000 - (minutes*60); // Remainder
-            String message = String.format("%sm %ss left", minutes, seconds);
-            logEntry(message);
-            if (millis <= 0) {
-                timerHandler.removeCallbacks(timerRunnable);
-                Context context = getApplicationContext();
-                Intent startIntent = new Intent(context, RingToneService.class);
-                startIntent.putExtra("ringtone-uri", ringtoneUri);
-                context.startService(startIntent);
+                long minutes = millis / 60000;
+                long seconds = millis / 1000 - (minutes * 60); // Remainder
+                String message = String.format("%sm %ss left", minutes, seconds);
+                logEntry(message);
+                if (millis <= 0) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    Context context = getApplicationContext();
+                    Intent startIntent = new Intent(context, RingToneService.class);
+                    startIntent.putExtra("ringtone-uri", ringtoneUri);
+                    context.startService(startIntent);
 
-            } else {
-                String text = "";
-                for (int i = 4; i >= 0; i--) {
-                    text = String.format("%s\n%s", log[i], text);
+                } else {
+                    String text = "";
+                    for (int i = 4; i >= 0; i--) {
+                        text = String.format("%s\n%s", log[i], text);
+                    }
+                    timerTextView.setText(text);
+
+                    timerHandler.postDelayed(this, 30000); //Check every 30 seconds when awake
                 }
-                timerTextView.setText(text);
-
-                timerHandler.postDelayed(this, 30000); //Check every 30 seconds.
+            } else {
+                timerHandler.postDelayed(this, 1200000); // Every 20 minutes when sleeping
             }
 
         }
@@ -121,6 +130,28 @@ public class MainActivity extends AppCompatActivity {
         String entry = String.format("%s: %s", format.format(calendar.getTime()), message);
         System.arraycopy(log, 0, log, 1, 4);
         log[0] = entry;
+    }
+
+    private boolean sleepTime() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        boolean sleep = false;
+
+            Calendar now = Calendar.getInstance();
+            Calendar wakeCalendar = (Calendar) now.clone();
+            Calendar sleepCalendar = (Calendar) now.clone();
+            String[] timeParts = WAKE_TIME.split(":"); //HH:mm
+            wakeCalendar.set(Calendar.HOUR_OF_DAY, Integer.valueOf(timeParts[0]));
+            wakeCalendar.set(Calendar.MINUTE, Integer.valueOf(timeParts[1]));
+            timeParts = SLEEP_TIME.split(":");
+            sleepCalendar.set(Calendar.HOUR_OF_DAY, Integer.valueOf(timeParts[0]));
+            sleepCalendar.set(Calendar.MINUTE, Integer.valueOf(timeParts[1]));
+            //logEntry(String.format("SleepTime: %s", format.format(sleepCalendar.getTime())));
+            //logEntry(String.format("wakeTime: %s", format.format(wakeCalendar.getTime())));
+            if (now.compareTo(sleepCalendar) >= 0 || now.compareTo(wakeCalendar) < 0) {
+                sleep = true;
+            }
+
+        return sleep;
     }
 
 
