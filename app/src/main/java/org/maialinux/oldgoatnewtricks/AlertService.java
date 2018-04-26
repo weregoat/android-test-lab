70fee099116d491fc8f4f7e774dd68dd70735bed
Ruggero Cucchiani <ruggero.cucchiani@exertisztorm.com>
2018-04-26 10:58:44 +0200

    update calculation of sleeping time
    I was not happy with the way the sleeping time was being handled.
    I switched to use full date-time (Joda) and shift the dates forwards a
    day once they have passed.

--- app/src/main/java/org/maialinux/oldgoatnewtricks/AlertService.java
+++ app/src/main/java/org/maialinux/oldgoatnewtricks/AlertService.java
@@ -14,9 +14,10 @@ import android.util.Log;
 import android.widget.Toast;
 
 
-import org.joda.time.Duration;
-import org.joda.time.Interval;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
 import org.joda.time.LocalTime;
+import org.joda.time.Period;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.joda.time.format.PeriodFormatter;
@@ -28,8 +29,8 @@ public class AlertService extends Service {
 
     private static final long INTERVAL = 3600000; // One hour
     private static final long ALERT_INTERVAL = 600000; // ten minutes
-    private static final String SLEEP_TIME = "20:00";
-    private static final String WAKE_TIME = "08:30";
+    private static final String SLEEP_TIME = "21:00";
+    private static final String WAKE_TIME = "08:00";
     private static final int MAX_ALERTS = 3;
     private static final long ALERT_DELAY = Math.round(ALERT_INTERVAL/MAX_ALERTS);
     public static final String BROADCAST_ACTION = "org.maialinux.oldgoatnewtricks.alert_service_broadcast";
@@ -43,11 +44,12 @@ public class AlertService extends Service {
     long expirationTime;
     int alertCounts = 0;
     long sleepDelay;
-    LocalTime wakeUpTime;
-    LocalTime sleepTime;
+
     Intent broadCastIntent;
     Intent serviceIntent;
     boolean accelerometerServiceStarted = false;
+    LocalDateTime wakeUpDateTime;
+    LocalDateTime sleepDateTime;
 
 
     Handler timerHandler = new Handler();
@@ -83,7 +85,7 @@ public class AlertService extends Service {
             } else {
                 delay = sleepDelay;
                 logEntry("Sleep time", false);
-                logEntry(String.format("Sleeping for %d seconds", sleepDelay/1000), false);
+                logEntry(String.format("Sleeping for %s seconds", String.valueOf(delay/1000)), false);
                 stopAccelerometerService();
             }
             if (alertCounts >= MAX_ALERTS) {
@@ -114,20 +116,17 @@ public class AlertService extends Service {
 
     @Override
     public void onCreate() {
+
         alertCounts = 0;
         Uri ringtoneURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         ringtone = RingtoneManager.getRingtone(getBaseContext(), ringtoneURI);
-        DateTimeFormatter formatter = ISODateTimeFormat.hourMinute();
-        wakeUpTime = LocalTime.parse(WAKE_TIME, formatter);
-        sleepTime = LocalTime.parse(SLEEP_TIME, formatter);
-        /* ensure that the local-time are in chronological order: wake up before sleeping */
-        if (wakeUpTime.isAfter(sleepTime)) {
-            sleepTime = LocalTime.parse(WAKE_TIME);
-            wakeUpTime = LocalTime.parse(SLEEP_TIME);
-        }
+
         broadCastIntent = new Intent(BROADCAST_ACTION);
         registerReceiver(broadcastReceiver, new IntentFilter(BROADCAST_ACTION));
         serviceIntent = new Intent(this, AccelerometerService.class);
+        parseSleepingTimes();
+        DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+        logEntry(String.format("Application will be sleeping from %s to %s", formatter.print(sleepDateTime), formatter.print(wakeUpDateTime)), false);
     }
 
 
@@ -170,29 +169,29 @@ public class AlertService extends Service {
     private boolean sleepTime() {
         boolean sleep = false;
         if (alertCounts == 0) { /* Never go to sleep if there are alerts */
-            LocalTime now = LocalTime.now();
-            if (now.isAfter(sleepTime) || now.isBefore(wakeUpTime)) {
+
+            LocalDateTime now = LocalDateTime.now();
+            /* If the wake-up time has passed shift it forwards */
+            if (now.isAfter(wakeUpDateTime)) {
+                wakeUpDateTime.plusDays(1);
+
+            }
+            if (now.isAfter(sleepDateTime) && now.isBefore(wakeUpDateTime)) {
                 sleep = true;
-                /* We are between midnight and wake-up time */
-                if (now.isBefore(sleepTime)) {
-                    sleepDelay = new Interval(now.getMillisOfDay(), wakeUpTime.getMillisOfDay()).toDurationMillis() + INTERVAL;
-                }
-                /* We are between sleep time and midnight */
-                if (now.isAfter(wakeUpTime)) {
-                    Interval pastSleepTime = new Interval(LocalTime.MIDNIGHT.getMillisOfDay(), now.getMillisOfDay());
-                    sleepDelay = pastSleepTime.toDurationMillis();
-                }
-                DateTimeFormatter format = ISODateTimeFormat.hourMinute();
-                logEntry(String.format("Sleep time %s", format.print(sleepTime)), false);
-                logEntry(String.format("Wakeup time %s", format.print(wakeUpTime)), false);
+                Period sleepPeriod = new Period(now, wakeUpDateTime);
+                sleepDelay = sleepPeriod.toStandardDuration().getMillis();
+                DateTimeFormatter format = ISODateTimeFormat.dateTimeNoMillis();
+                logEntry(String.format("Sleep time %s", format.print(sleepDateTime)), false);
+                logEntry(String.format("Wakeup time %s", format.print(wakeUpDateTime)), false);
                 PeriodFormatter formatter = new PeriodFormatterBuilder()
                         .appendHours()
                         .appendSuffix("h")
                         .appendMinutes()
                         .appendSuffix("m")
                         .toFormatter();
-                Duration sleepDuration = new Duration(sleepDelay);
-                logEntry(String.format("Sleeping for %s", formatter.print(sleepDuration.toPeriod())), false);
+                logEntry(String.format("Sleeping for %s", formatter.print(sleepPeriod)), false);
+                /* Set next sleeping time to tomorrow */
+                sleepDateTime = sleepDateTime.plusDays(1);
             } else {
                 sleepDelay = 0;
             }
@@ -243,6 +242,29 @@ public class AlertService extends Service {
     {
         stopService(serviceIntent);
         accelerometerServiceStarted = false;
+    }
+
+    private void parseSleepingTimes()
+    {
+        DateTimeFormatter formatter = ISODateTimeFormat.hourMinute();
+        LocalTime wakeUpTime = LocalTime.parse(WAKE_TIME, formatter);
+        LocalTime sleepTime = LocalTime.parse(SLEEP_TIME, formatter);
+        LocalDate today = new LocalDate();
+        sleepDateTime = new LocalDateTime()
+                .withDate(today.getYear(), today.getMonthOfYear(), today.getDayOfMonth())
+                .withMillisOfDay(sleepTime.getMillisOfDay());
+        wakeUpDateTime = new LocalDateTime()
+                .withDate(today.getYear(), today.getMonthOfYear(), today.getDayOfMonth())
+                .withMillisOfDay(wakeUpTime.getMillisOfDay());
+        /* If the application is started during the wake up period, shift the wake up a day forwards */
+        if (wakeUpDateTime.isBefore(LocalDateTime.now())) {
+            wakeUpDateTime = wakeUpDateTime.plusDays(1);
+        }
+
+
+
+
+
     }
 
 }
