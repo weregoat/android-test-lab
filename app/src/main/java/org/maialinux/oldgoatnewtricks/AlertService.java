c5c094bd95b2785c9b88c9a50fe735bfabbc2507
Ruggero Cucchiani <ruggero.cucchiani@exertisztorm.com>
2018-06-08 11:05:13 +0200

    prototype using JobScheduler
    Replaced the service for checking the sensors with a job(service)
    periodically ran by the alert service.
    Not sure yet if this is better or worse. Maybe it's just a
    different way to do it.

--- app/src/main/java/org/maialinux/oldgoatnewtricks/AlertService.java
+++ app/src/main/java/org/maialinux/oldgoatnewtricks/AlertService.java
@@ -1,14 +1,20 @@
 package org.maialinux.oldgoatnewtricks;
 
+import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -32,6 +38,8 @@ import org.joda.time.format.ISODateTimeFormat;
 import org.joda.time.format.PeriodFormatter;
 import org.joda.time.format.PeriodFormatterBuilder;
 
+import java.util.List;
+
 public class AlertService extends Service {
 
 
@@ -40,28 +48,29 @@ public class AlertService extends Service {
     private static final String SLEEP_TIME = "22:00";
     private static final String WAKE_TIME = "08:00";
     private static final int MAX_ALERTS = 3;
-    private static final long ALERT_DELAY = Math.round(ALERT_INTERVAL/MAX_ALERTS);
     public static final String BROADCAST_ACTION = "org.maialinux.oldgoatnewtricks.alert_service_broadcast";
     public static final String RESET_MESSAGE = "reset";
     public static final String END_MESSAGE = "stop";
+    public static final long MAX_DELAY = 300000; // 5 Minutes; to try catch sparse movements
+    public static final long MIN_DELAY = 60000; // One minute; to try to avoid flood of frequent movements
 
 
 
     //private final IBinder mBinder = new LocalBinder();
     private final String TAG = "AlertService";
     Ringtone ringtone;
-    long expirationTime;
+    long alertTime;
     int alertCounts = 0;
     long sleepDelay;
     long interval;
     long alertInterval;
     int maxAlerts = MAX_ALERTS;
     String phoneNumber;
+    boolean proximityRunning = false;
+    boolean sleep = false;
 
     Intent broadCastIntent;
-    Intent serviceIntent;
     Intent resetIntent;
-    boolean accelerometerServiceStarted = false;
     LocalDateTime wakeUpDateTime;
     LocalTime wakeUpTime;
     LocalTime sleepTime;
@@ -71,62 +80,63 @@ public class AlertService extends Service {
     NotificationCompat.Builder mBuilder;
     Notification notification;
 
+    private int mJobId = 0;
+    private ComponentName mServiceComponent;
+
+    private SensorManager sensorManager;
+
 
     Handler timerHandler = new Handler();
     Runnable timerRunnable = new Runnable() {
         @Override
         public void run() {
-            stopRingtone();
             long delay;
+            stopRingtone();
             DateTimeFormatter formatter = ISODateTimeFormat.hourMinute();
-
             if (isSleepTime() == false) {
-                long millis = expirationTime - System.currentTimeMillis();
+                startProximitySensor();
+                long millis = alertTime - System.currentTimeMillis();
                 if (millis <= 0) {
                     alertCounts++;
                     delay = 10000; // Rings for about ten seconds
                     stopRingtone();
                     playRingtone();
-                    expirationTime = System.currentTimeMillis() + alertInterval; // Reset the expiration time
-
+                    alertTime = System.currentTimeMillis() + alertInterval; // Reset the expiration time
                 } else {
                     long minutes = millis / 60000;
                     long seconds = millis / 1000 - (minutes * 60); // Remainder
                     String message = String.format("Alert timer: %sm %ss remaining", minutes, seconds);
                     logEntry(message, false);
+                    delay = millis;
                     if (alertCounts == 0) {
-                        delay = Math.round(interval / 30);
-                        if (millis < delay) {
-                            delay = millis;
+                        if (delay < MIN_DELAY) {
+                            delay = MIN_DELAY;
+                        } else if (delay > MAX_DELAY) {
+                            delay = MAX_DELAY;
                         }
-                    } else {
-                        delay = ALERT_DELAY;
                     }
-
                 }
-                if (accelerometerServiceStarted == false) {
-                    startAccelerometerService();
-                }
-
             } else {
                 delay = sleepDelay;
                 logEntry("Sleep time", false);
                 logEntry(String.format("Sleeping for %s seconds", String.valueOf(delay/1000)), false);
-                stopAccelerometerService();
+                stopSensorService();
+                sensorManager.unregisterListener(proximityEventListener);
             }
             if (alertCounts >= maxAlerts) {
                 stopRingtone();
                 sendSMS(phoneNumber, "Test SMS");
                 timerHandler.removeCallbacks(timerRunnable);
-                stopAccelerometerService();
                 stopSelf();
             }
 
-            String notificationText = String.format("Timer expiring at %s", formatter.print(new DateTime(expirationTime)));
+            String notificationText = String.format("Alert triggering at %s", formatter.print(new DateTime(alertTime)));
             if (alertCounts > 0) {
                 notificationText = String.format("Alert %d; %s", alertCounts, notificationText);
             }
             logEntry(notificationText, true);
+            logEntry(String.format("Next check at %s", formatter.print(new DateTime(System.currentTimeMillis() + delay))), false);
+            scheduleJob(delay/10);
             timerHandler.postDelayed(this, delay);
 
         }
@@ -146,11 +156,10 @@ public class AlertService extends Service {
         alertCounts = 0;
         Uri ringtoneURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         ringtone = RingtoneManager.getRingtone(getBaseContext(), ringtoneURI);
-        expirationTime = System.currentTimeMillis() + interval;
+        alertTime = System.currentTimeMillis() + interval;
 
         broadCastIntent = new Intent(BROADCAST_ACTION);
         registerReceiver(broadcastReceiver, new IntentFilter(BROADCAST_ACTION));
-        serviceIntent = new Intent(this, AccelerometerService.class);
         DateTimeFormatter formatter = ISODateTimeFormat.dateTimeNoMillis();
         logEntry(String.format("Application will be sleeping from %s to %s", formatter.print(sleepDateTime), formatter.print(wakeUpDateTime)), false);
         resetIntent = new Intent(AlertService.BROADCAST_ACTION);
@@ -170,6 +179,12 @@ public class AlertService extends Service {
         notification = mBuilder.build();
         startForeground(99, notification);
         logEntry(String.format("Interval: %d", interval/1000), false);
+        //startSensorService();
+        mServiceComponent = new ComponentName(this, SensorJobService.class);
+        //scheduleJob();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        startProximitySensor();
+
 
     }
 
@@ -197,7 +212,7 @@ public class AlertService extends Service {
     public void startTimer() {
         alertCounts = 0;
         reloadConfig();
-        expirationTime = System.currentTimeMillis() + interval;
+        alertTime = System.currentTimeMillis() + interval;
         timerHandler.removeCallbacks(timerRunnable);
         timerRunnable.run();
     }
@@ -212,7 +227,7 @@ public class AlertService extends Service {
     }
 
     private boolean isSleepTime() {
-        boolean sleep = false;
+        sleep = false;
         if (alertCounts == 0) { /* Never go to sleep if there are alerts */
             LocalDateTime now = LocalDateTime.now();
             /* We are between sleep time and wake-up time */
@@ -230,7 +245,7 @@ public class AlertService extends Service {
             }
             if (sleep == true) {
                 Period sleepPeriod = new Period(now, wakeUpDateTime);
-                sleepDelay = sleepPeriod.toStandardDuration().getMillis();
+                sleepDelay = sleepPeriod.toStandardDuration().getMillis(); // How long before the next check (not the same as alert)
                 DateTimeFormatter format = ISODateTimeFormat.dateTimeNoMillis();
                 logEntry(String.format("Sleep time %s", format.print(sleepDateTime)), false);
                 logEntry(String.format("Wakeup time %s", format.print(wakeUpDateTime)), false);
@@ -240,7 +255,7 @@ public class AlertService extends Service {
                         .appendMinutes()
                         .appendSuffix("m")
                         .toFormatter();
-                expirationTime = System.currentTimeMillis() + sleepDelay + interval;
+                alertTime = System.currentTimeMillis() + sleepDelay + interval; // Move the next alert to wake-up time + interval.
                 logEntry(String.format("Sleeping for %s", formatter.print(sleepPeriod)), true);
             } else {
                 sleepDelay = 0;
@@ -265,8 +280,10 @@ public class AlertService extends Service {
         this.stopRingtone();
         timerHandler.removeCallbacks(timerRunnable);
         unregisterReceiver(broadcastReceiver);
-        stopAccelerometerService();
         notificationManager.cancel(0);
+        JobScheduler tm = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        tm.cancelAll();
+        stopSensorService();
     }
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
@@ -275,14 +292,7 @@ public class AlertService extends Service {
            boolean resetMessage = intent.getBooleanExtra(RESET_MESSAGE, false);
            boolean endMessage = intent.getBooleanExtra(END_MESSAGE, false);
            if (resetMessage == true) {
-               expirationTime = System.currentTimeMillis() + interval;
-               logEntry(String.format("Interval: %d", interval/1000), false);
-               stopRingtone();
-               logEntry("Reset timer", false);
-               alertCounts = 0;
-               reloadConfig();
-               timerHandler.removeCallbacks(timerRunnable);
-               timerRunnable.run();
+               reset();
            }
            if (endMessage == true) {
                logEntry("Stop service", false);
@@ -292,16 +302,13 @@ public class AlertService extends Service {
         }
     };
 
-    private void startAccelerometerService()
-    {
-        startService(serviceIntent);
-        accelerometerServiceStarted = true;
-    }
 
-    private void stopAccelerometerService()
+    private void stopSensorService()
     {
+        JobScheduler tm = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        tm.cancelAll();
+        Intent serviceIntent = new Intent(this, SensorJobService.class);
         stopService(serviceIntent);
-        accelerometerServiceStarted = false;
     }
 
     private void calculateSleepDateTimes()
@@ -405,6 +412,69 @@ public class AlertService extends Service {
             logEntry("SMS sent", true);
         }
 
+    }
+
+    private void scheduleJob(long period) {
+        if (period < MIN_DELAY) {
+            period = MIN_DELAY;
+        }
+        Log.d(TAG, String.format("Period = %d seconds", Math.round(period/1000)));
+        JobScheduler tm = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo.Builder builder = new JobInfo.Builder(mJobId, mServiceComponent);
+
+        builder.setPeriodic(period);
+        //builder.setMinimumLatency(12000);
+        //builder.setOverrideDeadline(MIN_DELAY/2);
+        // Schedule job
+        Log.d(TAG, "Scheduling job");
+        tm.schedule(builder.build());
+    }
+
+    private final SensorEventListener proximityEventListener = new SensorEventListener() {
+
+
+        @Override
+
+        public void onSensorChanged(SensorEvent event) {
+            float proximityValue = event.values[0];
+            if (proximityValue < event.sensor.getMaximumRange()) {
+               Log.d(TAG, String.format("Proximity triggered with a value of %f", event.values[0]));
+               logEntry("Proximity sensor triggered", true);
+               reset();
+            }
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    private void reset() {
+        alertTime = System.currentTimeMillis() + interval;
+        logEntry(String.format("Interval: %d", interval/1000), false);
+        stopRingtone();
+        logEntry("Reset timer", false);
+        alertCounts = 0;
+        reloadConfig();
+        timerHandler.removeCallbacks(timerRunnable);
+        timerRunnable.run();
+    }
+
+    private void startProximitySensor() {
+        if (proximityRunning == false) {
+            List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+            for (int i = 0; i < sensorList.size(); i++) {
+                Log.d(TAG, sensorList.get(i).getName());
+                int sensorType = sensorList.get(i).getType();
+                if (sensorType == Sensor.TYPE_PROXIMITY) {
+                    sensorManager.registerListener(proximityEventListener, sensorManager.getDefaultSensor(sensorType), SensorManager.SENSOR_DELAY_NORMAL);
+                    proximityRunning = true;
+                    break;
+                }
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/org/maialinux/oldgoatnewtricks/SensorJobService.java
@@ -0,0 +1,197 @@
+package org.maialinux.oldgoatnewtricks;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+public class SensorJobService extends JobService {
+
+    public static final String TAG = SensorJobService.class.getSimpleName();
+    public static final long DURATION = 20000; // 20 seconds
+
+    private boolean reset = false;
+    private long duration = DURATION;
+    private SensorManager sensorManager;
+
+    Handler mHandler = new Handler();
+
+
+    private static final double ACCELERATION_THRESHOLD = 0.5f; // This much acceleration to trigger movement
+    private static final double GEOMAGNETIC_THRESHOLD = 10.0f; // This much change on any axis to trigger rest
+
+    /*
+     * time smoothing constant for low-pass filter
+     * 0 ≤ alpha ≤ 1 ; a smaller value basically means more smoothing
+     * See: http://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
+     * From: http://blog.thomnichols.org/2011/08/smoothing-sensor-data-with-a-low-pass-filter
+     */
+    private static final float ALPHA = 0.8f; // Threshold for low-pass filter
+
+    private float currentAcceleration;
+    private float lastAcceleration;
+    private float[] lastGeoMagneticValues;
+    private Intent broadcastIntent;
+
+
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        Log.d(TAG, "Job started");
+        checkSensors();
+
+        // Uses a handler to delay the execution of jobFinished().
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                jobFinished(jobParameters, false);
+            }
+        }, duration);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        reset = false;
+        sensorManager.unregisterListener(accelerometerEventListener);
+        sensorManager.unregisterListener(geoMagneticEventListener);
+        return false;
+    }
+
+
+
+    private void checkSensors() {
+
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (int i = 0; i < sensorList.size(); i++) {
+            Log.d(TAG, sensorList.get(i).getName());
+            int sensorType = sensorList.get(i).getType();
+            switch (sensorType) {
+                case Sensor.TYPE_ACCELEROMETER:
+                case Sensor.TYPE_ACCELEROMETER_UNCALIBRATED:
+                    sensorManager.registerListener(accelerometerEventListener, sensorManager.getDefaultSensor(sensorType), SensorManager.SENSOR_DELAY_NORMAL, 2000000000);
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    //case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
+                    sensorManager.registerListener(geoMagneticEventListener, sensorManager.getDefaultSensor(sensorType), SensorManager.SENSOR_DELAY_NORMAL, 2000000000);
+                    break;
+            }
+        }
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        broadcastIntent = new Intent(AlertService.BROADCAST_ACTION);
+        Log.i(TAG, "Service created");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(accelerometerEventListener);
+        sensorManager.unregisterListener(geoMagneticEventListener);
+        stopSelf();
+        Log.i(TAG, "Service destroyed");
+    }
+
+    private final SensorEventListener geoMagneticEventListener = new SensorEventListener() {
+
+
+        @Override
+
+        public void onSensorChanged(SensorEvent event) {
+            if (lastGeoMagneticValues != null && reset == false) {
+                for (int i = 0; i < 3; i++) {
+                    float delta = Math.abs(event.values[i] - lastGeoMagneticValues[i]);
+                    if (delta >= GEOMAGNETIC_THRESHOLD) {
+                        logEntry(String.format("Geomagnetic change of %f", delta), false);
+                        logEntry("Geomagnetic sensor triggered", true);
+                        sendResetBroadcast();
+                        break;
+                    }
+                }
+            }
+            lastGeoMagneticValues = event.values.clone();
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    private final SensorEventListener accelerometerEventListener = new SensorEventListener() {
+
+
+        @Override
+
+        /**
+         * @see http://en.wikipedia.org/wiki/Low-pass_filter#Algorithmic_implementation
+         * @see http://developer.android.com/reference/android/hardware/SensorEvent.html#values
+         * @see http://blog.thomnichols.org/2011/08/smoothing-sensor-data-with-a-low-pass-filter
+         * @see https://www.built.io/blog/applying-low-pass-filter-to-android-sensor-s-readings
+         */
+        public void onSensorChanged(SensorEvent event) {
+            float acceleration = 0.0f;
+            if (reset == false) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                lastAcceleration = currentAcceleration;
+                currentAcceleration = (float) Math.sqrt((double) (x * x + y * y + z * z)); // I don't care about specific axis
+                float delta = currentAcceleration - lastAcceleration;
+                acceleration = Math.abs(acceleration * ALPHA + delta); // Low-pass filter removing the high frequency noise
+                Log.d(TAG, String.format("acceleration: %f", acceleration));
+                if (acceleration >= ACCELERATION_THRESHOLD) {
+                    logEntry("Accelerometer sensor triggered", true);
+                    sendResetBroadcast();
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    private void logEntry(String message, boolean toast) {
+        if (toast) {
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        }
+        Log.d(TAG, message);
+        Intent logIntent = new Intent(AlertService.BROADCAST_ACTION);
+        logIntent.putExtra("message", message);
+        sendBroadcast(logIntent);
+    }
+
+    private void sendResetBroadcast() {
+        broadcastIntent.putExtra(AlertService.RESET_MESSAGE, true);
+        sendBroadcast(broadcastIntent);
+        Log.d(TAG, "Broadcast reset sent");
+        /* No need to keep listening */
+        sensorManager.unregisterListener(accelerometerEventListener);
+        sensorManager.unregisterListener(geoMagneticEventListener);
+        reset = false;
+    }
+}
